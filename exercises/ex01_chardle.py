b7d64aa4d5f9f52accd1384987cb3e119680dfa6
Kelly Ong <kong@unc.edu>
2022-08-25 17:12:27 -0400

    completed chardle exercise pt 1

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,52 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+
+__author__ = "730477669"
+
+word: str = input("Enter a 5-character word: ")
+length_word: int = len(word)
+if length_word != 5:
+    print("Error: Word must contain 5 characters")
+    exit()
+
+guess: str = input("Enter a single character: ")
+length_guess: int = len(guess)
+if length_guess != 1:
+    print("Error: Character must be a single character.")
+    exit()
+
+print("Searching for " + guess + " in " + word)
+
+i: int = 0
+count: int = 0
+if word[i] == guess:
+    print(guess + " found at index " + str(i))
+    count = count + 1
+
+i = i + 1
+if word[i] == guess:
+    print(guess + " found at index " + str(i))
+    count = count + 1
+
+i = i + 1
+if word[i] == guess:
+    print(guess + " found at index " + str(i))
+    count = count + 1
+
+i = i + 1
+if word[i] == guess:
+    print(guess + " found at index " + str(i))
+    count = count + 1
+
+i = i + 1
+if word[i] == guess:
+    print(guess + " found at index " + str(i))
+    count = count +1
+
+if count != 0:
+    print(str(count) + " instances of " + str(guess) + " found in " + word)
+else:
+    print("No instances of " + guess + " found in " + word)
+
+    
+
+
